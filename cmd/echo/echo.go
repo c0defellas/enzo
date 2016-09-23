03da79281a8cc508d5e8c34fe187936f1605ced4
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-09-22 21:48:57 -0300

    put echo on a separate function

--- cmd/echo/echo.go
+++ cmd/echo/echo.go
@@ -2,25 +2,31 @@ package main
 
 import "os"
 
-func main() {
-	addline := true
-	start := 1
-	last := len(os.Args) - 1
+func echo(args []string, newline bool) {
+	last := len(args) - 1
 
-	if last > 0 && os.Args[1] == "-n" {
-		addline = false
-		start = 2
-	}
-
-	for i := start; i < len(os.Args); i++ {
-		os.Stdout.Write([]byte(os.Args[i]))
+	for i := 0; i < len(args); i++ {
+		os.Stdout.Write([]byte(args[i]))
 
 		if i < last {
 			os.Stdout.Write([]byte{' '})
 		}
 	}
 
-	if addline {
+	if newline {
 		os.Stdout.Write([]byte{0x0a})
 	}
+}
+
+func main() {
+	newline := true
+	start := 1
+	last := len(os.Args) - 1
+
+	if last > 0 && os.Args[1] == "-n" {
+		newline = false
+		start = 2
+	}
+
+	echo(os.Args[start:], newline)
 }
