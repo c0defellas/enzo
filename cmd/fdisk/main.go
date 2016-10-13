e46151cbadd058b9054d62ec9dbd3925d681d722
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-10-13 02:52:26 -0300

    add fdisk utility

---
+++ cmd/fdisk/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	// Classical MBR structure
+	CPart1 = 0x1be // 16 bytes each
+	CPart2 = 0x1ce
+	CPart3 = 0x1de
+	CPart4 = 0x1ee
+
+	PEntrySZ = 16
+
+	Magic1Off = 0x1fe
+	Magic2Off = 0x1ff
+
+	// MBR magic numbers
+	Magic1 = 0x55
+	Magic2 = 0xaa
+)
+
+func diskinfo(fname string) error {
+	file, err := os.Open(fname)
+
+	if err != nil {
+		return err
+	}
+
+	var mbr [512]byte
+
+	_, err = file.Read(mbr[:])
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	if mbr[Magic1Off] != Magic1 &&
+		mbr[Magic2Off] != Magic2 {
+		return fmt.Errorf("no MBR found\n")
+	}
+
+	var part *partition
+	var empty bool
+
+	if part, err, empty = NewPartition(mbr[CPart1 : CPart1+PEntrySZ]); err != nil {
+		return err
+	}
+
+	if !empty {
+		fmt.Printf("%s\n", part)
+	}
+
+	if part, err, empty = NewPartition(mbr[CPart2 : CPart2+PEntrySZ]); err != nil {
+		return err
+	}
+
+	if !empty {
+		fmt.Printf("%s\n", part)
+	}
+
+	if part, err, empty = NewPartition(mbr[CPart3 : CPart3+PEntrySZ]); err != nil {
+		return err
+	}
+
+	if !empty {
+		fmt.Printf("%s\n", part)
+	}
+
+	if part, err, empty = NewPartition(mbr[CPart4 : CPart4+PEntrySZ]); err != nil {
+		return err
+	}
+
+	if !empty {
+		fmt.Printf("%s\n", part)
+	}
+
+	return nil
+}
+
+func isPartEmpty(buf []byte) bool {
+	for i := 0; i < 16; i++ {
+		if buf[i] != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	flag.Parse()
+	diskpaths := flag.Args()
+
+	if len(diskpaths) == 0 {
+		fmt.Fprintf(os.Stderr, "Usage: %s dev1 dev2 ...\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	for _, disk := range diskpaths {
+		err := diskinfo(disk)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to get MBR/GPT info: %s", err)
+			os.Exit(1)
+		}
+	}
+
+}
---
+++ cmd/fdisk/partition.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+type (
+	// cylinder-head-sector
+	chs struct {
+		head, sector uint8
+		cylinder     uint16
+	}
+
+	status byte
+	typ    byte
+
+	partition struct {
+		number   uint8
+		status   status
+		begin    chs
+		typ      typ
+		end      chs
+		lba      int32
+		nsectors int32
+	}
+)
+
+func (chs chs) String() string {
+	return fmt.Sprintf("%d/%d/%d", chs.cylinder, chs.head, chs.sector)
+}
+
+func (t typ) String() string {
+	switch t {
+	case 0x83:
+		return fmt.Sprintf("%x (Linux)", int(t))
+	}
+	return "unknown"
+
+}
+
+func (st status) String() string {
+	err := ""
+	if st&0x80 != 0 && st&0x80 != 0x80 {
+		err += " (wrong)"
+	}
+
+	switch st >> 7 {
+	case 1:
+		return "active" + err
+	}
+
+	return "inactive" + err
+}
+
+func (p partition) String() string {
+	format := `Partition #%d
+Status: %s
+FS type: %s
+First C/H/S: %s
+Last C/H/S: %s
+LBA: %d
+Number of sectors: %d
+`
+	return fmt.Sprintf(format, p.number, p.status, p.typ, p.begin, p.end, p.lba, p.nsectors)
+}
+
+func NewPartition(entry []byte) (*partition, error, bool) {
+	part := &partition{}
+
+	if len(entry) != 16 {
+		return nil, fmt.Errorf("Invalid partition entry: %v", entry), false
+	}
+
+	if isPartEmpty(entry) {
+		return nil, nil, true
+	}
+
+	part.status = status(entry[0])
+	part.begin.head = uint8(entry[1])
+	part.begin.sector = uint8(entry[2])
+	part.begin.cylinder = uint16(entry[3])
+	part.typ = typ(entry[4])
+	part.end.head = uint8(entry[5])
+	part.end.sector = uint8(entry[6] & 0x3f) // sector in bits 5-0
+	part.end.cylinder = uint16(entry[6]&0xc0)<<2 | uint16(entry[7])
+
+	buf := bytes.NewBuffer(entry[8:12])
+	err := binary.Read(buf, binary.LittleEndian, &part.lba)
+
+	if err != nil {
+		return nil, err, false
+	}
+
+	buf = bytes.NewBuffer(entry[12:16])
+	err = binary.Read(buf, binary.LittleEndian, &part.nsectors)
+
+	return part, err, false
+}
